ab5ba3b1612e9becbc16871ff9ff871e052e5896
ZachTheis <70988581+ZachTheis@users.noreply.github.com>
2021-12-07 19:17:16 -0500

    Update images
    Add username and description to image search

--- src/app.js
+++ src/app.js
@@ -31,18 +31,30 @@ form.addEventListener('submit', async (event) => {
     postImg.src = dataObj.urls.small;
     postImg.alt = dataObj.alt_description;
 
-    container.appendChild(clone);
-  });
-
-  /*
+    /*
     Add an attribution statement below the image using the
     postUser element and the photographer's name from dataObj
    */
+    const user = clone.querySelector('.post__user');
+    let attribution = 'Photo by: ';
+    const userName = dataObj.user.username;
+    attribution = attribution.concat(userName);
+    user.innerText = attribution;
 
-  /*
+    /*
     Check the description of the post. If it's not null and less than 100 characters,
     add the description from dataObj to the post. If it's more than 100 characters,
     add the first 100 characters of the description from dataObj to the post followed by
     an ellipsis (...)
-  */
+    */
+    const postDesc = clone.querySelector('.post__desc');
+    let description = dataObj.description;
+    if (description !== null && description.length > 100) {
+      description = description.slice(0, 100);
+      description = description.concat('...');
+    }
+    postDesc.innerText = description;
+
+    container.appendChild(clone);
+  });
 });
